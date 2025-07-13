9a4964b23ccbf10668dcee313ddd5ecad2d5991d
muharya55 <farhansyahputra9901@gmail.com>
2025-07-13 20:33:20 +0700

    perbaikan

--- src/app/api/artikel/[slug]/route.ts
+++ src/app/api/artikel/[slug]/route.ts
@@ -12,7 +12,7 @@ function serialize<T>(obj: T): T {
   );
 }
 
-type Params = { params: { slug: string } }
+// type Params = { params: { slug: string } }
 
 export async function GET( request: NextRequest,
   context: {

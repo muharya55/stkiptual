cc9bc0c825aa85eb62a20b5edc373a001f552aa8
muharya55 <farhansyahputra9901@gmail.com>
2025-07-20 15:31:42 +0700

    perbaikan

--- src/app/berita-list/page.tsx
+++ src/app/berita-list/page.tsx
@@ -1,11 +1,15 @@
 import ListBerita from "app/components/ListBerita"
+import { Suspense } from "react";
 
- 
+ export const dynamic = 'force-dynamic'; // <- WAJIB
+
 const Page = () => {
   return (
     <>
         <section className="courses-page courses-list-v2 mt-30px mb-80px">
-            <ListBerita/>
+              <Suspense fallback={<p>Memuat halaman berita...</p>}>
+                <ListBerita/>
+             </Suspense>
         </section>
 
     </>

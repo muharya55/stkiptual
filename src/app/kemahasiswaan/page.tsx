83ab87dda3caf50816d9a193c640203e293f10e1
muharya55 <farhansyahputra9901@gmail.com>
2025-07-29 20:40:29 +0700

    perbaikan

--- src/app/kemahasiswaan/page.tsx
+++ src/app/kemahasiswaan/page.tsx
@@ -2,9 +2,7 @@
 // app/data/sidebarMenu.ts
 
 import KemahasiswaanSection from "app/section/KemahasiswaanSection";
-import Image from "next/image";
-import Link from "next/link";
-
+ 
 export type MenuItem = {
   label: string;
   href: string;
